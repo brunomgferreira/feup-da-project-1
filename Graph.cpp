9dd67bfb846c249466adb3a0a68834c1949600e6
brunomgferreira <brunomgferreira17@gmail.com>
2024-03-03 09:06:41 +0000

    basic graph structure is done

--- Graph.cpp
+++ Graph.cpp
@@ -1,1 +1,238 @@
 #include "Graph.h"
+
+
+/************************* Vertex  **************************/
+
+void Vertex::deleteEdge(Edge *edge) {
+    Vertex *dest = edge->getDest();
+    // Remove the corresponding edge from the incoming list
+    auto it = dest->incoming.begin();
+    while (it != dest->incoming.end()) {
+        if ((*it)->getOrig()->getCode() == this->code) {
+            it = dest->incoming.erase(it);
+        }
+        else {
+            it++;
+        }
+    }
+    delete edge;
+}
+
+Vertex::Vertex(string code, VertexType type) : code(code), type(type) {}
+
+bool Vertex::operator<(Vertex & vertex) const {
+    return this->dist < vertex.dist;
+}
+
+string Vertex::getCode() const {
+    return this->code;
+}
+
+VertexType Vertex::getType() const {
+    return this->type;
+}
+
+vector<Edge *> Vertex::getAdj() const {
+    return this->adj;
+}
+
+bool Vertex::isVisited() const {
+    return this->visited;
+}
+
+bool Vertex::isProcessing() const {
+    return this->processing;
+}
+
+unsigned int Vertex::getIndegree() const {
+    return this->indegree;
+}
+
+double Vertex::getDist() const {
+    return this->dist;
+}
+
+Edge *Vertex::getPath() const {
+    return this->path;
+}
+
+vector<Edge *> Vertex::getIncoming() const {
+    return this->incoming;
+}
+
+void Vertex::setCode(string code) {
+    this->code = code;
+}
+
+void Vertex::setType(VertexType type) {
+    this->type = type;
+}
+
+void Vertex::setVisited(bool visited) {
+    this->visited = visited;
+}
+
+void Vertex::setProcesssing(bool processing) {
+    this->processing = processing;
+}
+
+void Vertex::setIndegree(unsigned int indegree) {
+    this->indegree = indegree;
+}
+
+void Vertex::setDist(double dist) {
+    this->dist = dist;
+}
+
+void Vertex::setPath(Edge *path) {
+    this->path = path;
+}
+
+Edge * Vertex::addEdge(Vertex *dest, double c, string code) {
+    auto newEdge = new Edge(code, this, dest, c);
+    adj.push_back(newEdge);
+    dest->incoming.push_back(newEdge);
+    return newEdge;
+}
+
+bool Vertex::removeEdge(string code) {
+    bool removedEdge = false;
+    auto it = adj.begin();
+    while (it != adj.end()) {
+        Edge *edge = *it;
+        Vertex *dest = edge->getDest();
+        if (dest->getCode() == code) {
+            it = adj.erase(it);
+            deleteEdge(edge);
+            removedEdge = true; // allows for multiple edges to connect the same pair of vertices (multigraph)
+        }
+        else {
+            it++;
+        }
+    }
+    return removedEdge;
+}
+
+void Vertex::removeOutgoingEdges() {
+    auto it = adj.begin();
+    while (it != adj.end()) {
+        Edge *edge = *it;
+        it = adj.erase(it);
+        deleteEdge(edge);
+    }
+}
+
+
+/********************** Edge  ****************************/
+
+Edge::Edge(string code, Vertex *orig, Vertex *dest, double capacity) : code(code), orig(orig), dest(dest), capacity(capacity) {}
+
+Vertex * Edge::getDest() const {
+    return this->dest;
+}
+
+double Edge::getCapacity() const {
+    return this->capacity;
+}
+
+bool Edge::isSelected() const {
+    return this->selected;
+}
+
+Vertex * Edge::getOrig() const {
+    return this->orig;
+}
+
+Edge *Edge::getReverse() const {
+    return this->reverse;
+}
+
+double Edge::getFlow() const {
+    return this->flow;
+}
+
+void Edge::setSelected(bool selected) {
+    this->selected = selected;
+}
+
+void Edge::setReverse(Edge *reverse) {
+    this->reverse = reverse;
+}
+
+void Edge::setFlow(double flow) {
+    this->flow = flow;
+}
+
+
+/********************** Graph  ****************************/
+
+Vertex *Graph::findVertex(const string &code) const {
+    auto it = this->vertices.find(code);
+    if (it != this->vertices.end()) {
+        return it->second;
+    }
+    return nullptr;
+}
+
+bool Graph::addVertex(const string &code, const VertexType &type) {
+    if(findVertex(code) == nullptr) {
+        Vertex *newVertex = new Vertex(code, type);
+        this->vertices.insert({code, newVertex});
+        return true;
+    }
+    return false;
+}
+
+bool Graph::addEdge(const string &sourc, const string &dest, double c) {
+    Vertex *originVertex = findVertex(sourc);
+    Vertex *destVertex = findVertex(dest);
+
+    if (originVertex && destVertex) {
+        originVertex->addEdge(destVertex, c);
+        return true;
+    }
+
+    return false;
+}
+
+bool Graph::removeEdge(const string &source, const string &dest) {
+    Vertex * srcVertex = findVertex(source);
+    if (srcVertex == nullptr) {
+        return false;
+    }
+    return srcVertex->removeEdge(dest);
+}
+
+bool Graph::addBidirectionalEdge(const string &sourc, const string &dest, double c) {
+    auto v1 = findVertex(sourc);
+    auto v2 = findVertex(dest);
+    if (v1 == nullptr || v2 == nullptr)
+        return false;
+    auto e1 = v1->addEdge(v2, c);
+    auto e2 = v2->addEdge(v1, c);
+    e1->setReverse(e2);
+    e2->setReverse(e1);
+    return true;
+}
+
+int Graph::getNumVertex() const {
+    return this->vertices.size();
+}
+
+unordered_map<string, Vertex *> Graph::getVertexSet() const {
+    return this->vertices;
+}
+
+vector<string> Graph::dfs() const {}
+
+vector<string> Graph::dfs(const string & source) const {}
+
+void Graph::dfsVisit(Vertex *v,  vector<string> & res) const {}
+
+vector<string> Graph::bfs(const string & source) const {}
+
+bool Graph::isDAG() const {}
+
+bool Graph::dfsIsDAG(Vertex *v) const {}
+
+vector<string> Graph::topsort() const {}
--- Graph.h
+++ Graph.h
@@ -2,9 +2,134 @@
 #define WATER_SUPPLY_ANALYSIS_SYSTEM_GRAPH_H
 
 
-class Graph {
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <limits>
+#include <algorithm>
 
+using namespace std;
+
+class Edge;
+
+#define INF std::numeric_limits<double>::max()
+
+/************************* Vertex  **************************/
+
+enum class VertexType { WaterReservoir, PumpingStation, DeliveryStation };
+
+class Vertex {
+private:
+    string code;            // code of the node
+    VertexType type;        // type of the node
+    vector<Edge *> adj;  // outgoing edges
+
+    // auxiliary fields
+    bool visited = false; // used by DFS, BFS, Prim ...
+    bool processing = false; // used by isDAG (in addition to the visited attribute)
+    unsigned int indegree; // used by topsort
+    double dist = 0;
+    Edge *path = nullptr;
+
+    vector<Edge *> incoming; // incoming edges
+
+    void deleteEdge(Edge *edge);
+
+public:
+    Vertex(string code, VertexType type);
+    bool operator<(Vertex & vertex) const;
+
+    string getCode() const;
+    VertexType getType() const;
+    vector<Edge *> getAdj() const;
+    bool isVisited() const;
+    bool isProcessing() const;
+    unsigned int getIndegree() const;
+    double getDist() const;
+    Edge *getPath() const;
+    vector<Edge *> getIncoming() const;
+
+    void setCode(string code);
+    void setType(VertexType type);
+    void setVisited(bool visited);
+    void setProcesssing(bool processing);
+    void setIndegree(unsigned int indegree);
+    void setDist(double dist);
+    void setPath(Edge *path);
+    Edge * addEdge(Vertex *dest, double c, string code = nullptr);
+    bool removeEdge(string code);
+    void removeOutgoingEdges();
 };
 
+/********************** Edge  ****************************/
+
+class Edge {
+private:
+    string code; // pipe code
+    Vertex *dest; // destination vertex
+    double capacity; // edge capacity
+
+    // auxiliary fields
+    bool selected = false;
+
+    // used for bidirectional edges
+    Vertex *orig;
+    Edge *reverse = nullptr;
+
+    double flow; // for flow-related problems
+
+public:
+    Edge(string code, Vertex *orig, Vertex *dest, double capacity);
+
+    Vertex * getDest() const;
+    double getCapacity() const;
+    bool isSelected() const;
+    Vertex * getOrig() const;
+    Edge *getReverse() const;
+    double getFlow() const;
+
+    void setSelected(bool selected);
+    void setReverse(Edge *reverse);
+    void setFlow(double flow);
+};
+
+/********************** Graph  ****************************/
+
+class Graph {
+private:
+    unordered_map<string, Vertex *> vertices;    // vertex set
+
+public:
+    /*
+    * Auxiliary function to find a vertex with a given the content.
+    */
+    Vertex *findVertex(const string &code) const;
+    /*
+     *  Adds a vertex with a given content or info (in) to a graph (this).
+     *  Returns true if successful, and false if a vertex with that content already exists.
+     */
+    bool addVertex(const string &code, const VertexType &type);
+
+    /*
+     * Adds an edge to a graph (this), given the contents of the source and
+     * destination vertices and the edge capacity (c).
+     * Returns true if successful, and false if the source or destination vertex does not exist.
+     */
+    bool addEdge(const string &sourc, const string &dest, double c);
+    bool removeEdge(const string &source, const string &dest);
+    bool addBidirectionalEdge(const string &sourc, const string &dest, double c);
+
+    int getNumVertex() const;
+    unordered_map<string, Vertex *> getVertexSet() const;
+
+    vector<string> dfs() const;
+    vector<string> dfs(const string & source) const;
+    void dfsVisit(Vertex *v,  vector<string> & res) const;
+    vector<string> bfs(const string & source) const;
+
+    bool isDAG() const;
+    bool dfsIsDAG(Vertex *v) const;
+    vector<string> topsort() const;
+};
 
 #endif //WATER_SUPPLY_ANALYSIS_SYSTEM_GRAPH_H
